1ed56bd4cf2be10c104af0b016532f7286941350
alexander <alexander@debian81.debian81>
2014-03-26 00:17:09 +0400

    first version upload

---
+++ l4/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: alexander
+ *
+ * Created on 26 марта 2014 г., 0:06
+ */
+#include<cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout<<"Hello, World!"<<endl;
+    return 0;
+}
+
