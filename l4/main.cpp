397e21ceb13ab4a77141971d13fbc0f79ac37d86
alexander <alexander@debian168>
2014-03-26 14:45:54 +0400

    Some games with datatypes

--- l4/main.cpp
+++ l4/main.cpp
@@ -6,6 +6,10 @@
  */
 #include<cstdlib>
 #include <iostream>
+#include <math.h>
+#include <typeinfo>
+
+#define _USE_MATH_DEFINES
 
 using namespace std;
 
@@ -13,7 +17,46 @@ using namespace std;
  * 
  */
 int main(int argc, char** argv) {
-    cout<<"Hola, Mundo!"<<endl;
+    cout<<"Lesson 2: Datatypes"<<endl;
+    int    _a;
+    int    _b=7;
+    int    _c=-54.5;
+    string _d="I'm a string";
+    char   _e=_d[2];
+    long   _f=8678578435737685487686;
+    double _g=M_PI;
+    bool   _h;
+    
+    
+    
+    cout<<"VALUES OF THESE VARIABLES"<<endl;
+    cout<<_a<<endl;
+    cout<<_b<<endl;
+    cout<<_c<<endl;
+    cout<<_d<<endl;
+    cout<<_e<<endl;
+    cout<<_f<<" instead of 8678578435737685487686"<<endl;
+    cout<<_g<<endl;
+    cout<<_h<<endl;
+    cout<<"-------------------"<<endl;
+      cout<<"GAMES WITH BOOLEAN VARIABLES"<<endl;
+    _h=0;
+    cout<<"assigned to 0 "<<_h<<endl;
+    
+    _h=1;
+    cout<<"assigned to 1 "<<_h<<endl;
+    
+    _h=21;
+    cout<<boolalpha<<"assigned to 21 "<<_h<<" but is boolean "<<(typeid(_h) ==typeid(bool))<<endl;
+    
+    _h=true;
+    cout<<"assigned to true "<<_h<<endl;
+    
+    _h=false;
+    cout<<"assigned to false "<<_h<<endl;
+    
+   
+    
     return 0;
 }
 
